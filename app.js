c6c5f920a43d8b5b6810cb8ca219348b49be2072
muhammad adhil <muhammadadhil934@gmail.com>
2024-02-10 18:15:52 +0530

    first commit:added all files

---
+++ app.js
@@ -0,0 +1,49 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+
+const adminRouter = require('./routes/admin');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+
+// app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRouter);
+app.use('/',usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log('error page working !!');
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const port=4000
+
+app.listen(port,()=>console.log(`server running on the port http://localhost:${port}`));
+
+
+module.exports = app;
